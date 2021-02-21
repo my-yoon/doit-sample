9c778f7368f78f350b3a2d1679ccd761c1999f5e
Yoon <yoon.kim@brabbit.com>
2021-02-22 02:02:50 +0900

    init

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
-import Vue from 'vue'
 import App from './App.vue'
-
+import Vue from 'vue'
+import router from './router';
+import { BootstrapVue} from 'bootstrap-vue'
+import VueSlickCarousel from 'vue-slick-carousel'
+Vue.component('VueSlickCarousel', VueSlickCarousel);
+Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
+
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
